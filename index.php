ad1d1ad79383eecbb81a453c97ab8623c11efebe
micaeleee <49121243+micaeleee@users.noreply.github.com>
2019-10-07 10:46:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ session_start();
             <div class="container has-text-centered">
                 <div class="column is-4 is-offset-4">
                     <h3 class="title has-text-grey">Sistema de Login</h3>
-                    <h3 class="title has-text-grey"><a  target="_blank">Biblioteca</a></h3>
+                    <h3 class="title has-text-grey"><a  target="_blank">Aluno</a></h3>
                     <div class="notification is-danger">
                       <p>ERRO: Usuário ou senha inválidos.</p>
                     </div>
